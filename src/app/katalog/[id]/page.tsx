d86effeb09b9ce1d508af3296252648220281e83
ahnafzaki12 <ahnafzaki.b@gmail.com>
2025-08-15 20:39:44 +0700

    navbar

--- src/app/katalog/[id]/page.tsx
+++ src/app/katalog/[id]/page.tsx
@@ -89,7 +89,7 @@ const ProductDetailPage = () => {
 
     return (
         <>
-        
+        <Navbar />
         <div className="min-h-screen bg-gray-50">
             <div className="max-w-7xl mx-auto px-4 py-6">
                 {/* Product Detail Container */}
